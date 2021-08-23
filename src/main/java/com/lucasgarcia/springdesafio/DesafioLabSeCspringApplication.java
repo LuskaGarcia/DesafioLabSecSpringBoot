b8ba1a536268f3d4d8709634fff2f4a493643283
lucasflorianogarcia@gmail.com <lucasflorianogarcia@gmail.com>
2021-08-23 11:19:56 -0300

    Projeto criado

---
+++ src/main/java/com/lucasgarcia/springdesafio/DesafioLabSeCspringApplication.java
@@ -0,0 +1,13 @@
+package com.lucasgarcia.springdesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioLabSeCspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioLabSeCspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasgarcia/springdesafio/DesafioLabSeCspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasgarcia.springdesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioLabSeCspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
